75694987896416c4dc2fe5f5d325ee4294187568
Taytay884 <Taytay884@yahoo.com>
2018-06-07 14:33:11 +0300

    CHECKING UPDATE

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,38 +1,40 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-import './Header.css';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 class Header extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isSideMenuOpen: false,
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSideMenuOpen: false
+    };
+  }
 
-    toggleMenu = (e) => {
-        e.preventDefault();
-        this.setState({ isSideMenuOpen: !this.state.isSideMenuOpen })
-    }
+  toggleMenu = e => {
+    e.preventDefault();
+    this.setState({ isSideMenuOpen: !this.state.isSideMenuOpen });
+  };
 
-    render() {
-        return (
-            <header className='App-header flex space-between align-center justify-center'>
-                <h1 className='App-title'>CrypTop</h1>
-                <div className='hamburger' onClick={this.toggleMenu}>
-                    {(this.state.isSideMenuOpen) ?
-                        <i className='menu-icon-active'></i> :
-                        <i className='menu-icon'></i>}
-                </div>
-                <div className={(this.state.isSideMenuOpen) ? 'side-bar menu' : 'nav-bar menu'}>
+  render() {
+    return (
+      <header className="App-header flex space-between align-center justify-center">
+        <h1 className="App-title">CrypTop</h1>
+        <div className="hamburger" onClick={this.toggleMenu}>
+          {this.state.isSideMenuOpen ? (
+            <i className="menu-icon-active" />
+          ) : (
+            <i className="menu-icon" />
+          )}
+        </div>
+        {/* <div className={(this.state.isSideMenuOpen) ? 'side-bar menu' : 'nav-bar menu'}>
                     <ul className='flex space-between'>
                         <li><NavLink exact activeClassName='selected' to='/'>Home</NavLink></li>
                         <li><NavLink exact activeClassName='selected' to='/contact'>Contacts</NavLink></li>
                         <li><NavLink exact activeClassName='selected' to='/login'>Login</NavLink></li>
                     </ul>
-                </div>
-            </header >
-        );
-    }
+                </div> */}
+      </header>
+    );
+  }
 }
 export default Header;
